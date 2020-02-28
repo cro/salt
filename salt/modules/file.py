dde0929688208f93b094becb364323fc151cc67d
C. R. Oldham <cro@ncbt.org>
2020-02-28 12:14:27 -0700

    Moved nametuple out of first import.

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -30,9 +30,10 @@ import glob
 import hashlib
 import mmap
 try:
-    from collections.abc import Iterable, Mapping, namedtuple
+    from collections.abc import Iterable, Mapping
 except ImportError:
-    from collectionsimport Iterable, Mapping, namedtuple
+    from collections import Iterable, Mapping
+from collections import namedtuple
 from functools import reduce  # pylint: disable=redefined-builtin
 
 # pylint: disable=import-error,no-name-in-module,redefined-builtin
